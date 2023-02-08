91bdb0a897e9ef935dcb89d8134110cf3f51043e
David Bohn <david.bohn@tts-security.com>
2023-02-08 14:44:18 +0100

    Update run script

--- run.sh
+++ run.sh
@@ -3,8 +3,9 @@
 echo "Run using anonymous class migrations"
 rm database/migrations/*.php
 cp database/migrations/new/*.php database/migrations
-phpunit
+php vendor/bin/phpunit
 echo "Run using classic class migrations"
 rm database/migrations/*.php
 cp database/migrations/classic/*.php database/migrations
-phpunit
+php vendor/bin/phpunit
+rm database/migrations/*.php
